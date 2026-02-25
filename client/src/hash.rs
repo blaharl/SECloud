9913d8f531d9ce60ebe7278af218772e9036fea2
pointing <pointing@disroot.org>
2026-02-25 18:51:28 +0900

    modularized hash functions

--- client/src/hash.rs
+++ client/src/hash.rs
@@ -8,7 +8,31 @@ use crate::error::ErrorMessage;
 
 const MAX_PASSWORD_LENGTH: usize = 255;
 
-pub fn hash(
+pub fn sha256_hash(input: impl Into<String>) -> Result<String, ErrorMessage> {
+    let mut hasher = Sha256::new();
+    hasher.update(input.into());
+    let username_hash = hasher.finalize();
+    String::from_utf8(format!("{:x}", username_hash).into()).map_err(|_| ErrorMessage::HashingError)
+}
+
+pub fn argon2id_hash(input: &[u8], salt: impl Into<String>) -> Result<String, ErrorMessage> {
+    let argon2id = Argon2::new(
+        argon2::Algorithm::Argon2id,
+        argon2::Version::V0x13,
+        argon2::Params::new(19456, 2, 1, None).map_err(|_| ErrorMessage::HashingError)?,
+    );
+
+    let salt = SaltString::from_b64(&salt.into()).map_err(|_| ErrorMessage::HashingError)?;
+
+    Ok(argon2id
+        .hash_password(input, &salt)
+        .map_err(|_| ErrorMessage::HashingError)?
+        .to_string())
+}
+
+/// hash password with argon2d (default params)
+/// username (sha256) used as salt
+pub fn password_hash(
     password: impl Into<String>,
     username: impl Into<String>,
 ) -> Result<String, ErrorMessage> {
@@ -18,24 +42,9 @@ pub fn hash(
         return Err(ErrorMessage::EmptyPassword);
     }
 
-    let argon2 = Argon2::new(
-        argon2::Algorithm::Argon2id,
-        argon2::Version::V0x13,
-        argon2::Params::new(19456, 2, 1, None).map_err(|_| ErrorMessage::HashingError)?,
-    );
-
-    let mut hasher = Sha256::new();
-    hasher.update(username.into());
-    let username_hash = hasher.finalize();
-    let username_hash = String::from_utf8(format!("{:x}", username_hash).into())
+    let username_hash = sha256_hash(username)?;
+    let hashed_password = argon2id_hash(password.as_bytes(), username_hash)
         .map_err(|_| ErrorMessage::HashingError)?;
-    eprintln!("{}", username_hash);
-    let salt = SaltString::from_b64(&username_hash).map_err(|_| ErrorMessage::HashingError)?;
-
-    let hashed_password = argon2
-        .hash_password(password.as_bytes(), &salt)
-        .map_err(|_| ErrorMessage::HashingError)?
-        .to_string();
 
     if hashed_password.len() > MAX_PASSWORD_LENGTH {
         return Err(ErrorMessage::ExceededMaxPasswordLength(MAX_PASSWORD_LENGTH));
@@ -51,11 +60,7 @@ mod tests {
     #[test]
     fn test_sha256() {
         let username = "username";
-
-        let mut hasher = Sha256::new();
-        hasher.update(username);
-        let username_hash = hasher.finalize();
-        let hash_str = String::from_utf8(format!("{:x}", username_hash).into()).unwrap();
+        let hash_str = sha256_hash(username).unwrap();
         assert_eq!(
             hash_str,
             "16f78a7d6317f102bbd95fc9a4f3ff2e3249287690b8bdad6b7810f82b34ace3"
@@ -66,7 +71,7 @@ mod tests {
     fn test_hash() {
         let username = "username";
         let password = "password";
-        let hashed_result = hash(password, username).unwrap();
+        let hashed_result = password_hash(password, username).unwrap();
         assert_eq!(
             hashed_result,
             "$argon2id$v=19$m=19456,t=2,p=1$16f78a7d6317f102bbd95fc9a4f3ff2e3249287690b8bdad6b7810f82b34ace3$lzrW+r2NWIW2MRpY5u2aYgg4kPckPBeILt3RsgpSKR8"
